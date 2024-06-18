c85dd78e83c8483a093b4f40208e6886ebe9a1f6
pie <yejinh_h@naver.com>
2024-06-18 15:17:16 +0900

    #[refactor] 11 : Partner 클래스도 MapStruct를 사용해 Mapper 클래스와 매핑될 Dto 클래스 생성

--- src/main/java/dev/practice/ecommerce/interfaces/partner/PartnerApiController.java
+++ src/main/java/dev/practice/ecommerce/interfaces/partner/PartnerApiController.java
@@ -1,6 +1,9 @@
 package dev.practice.ecommerce.interfaces.partner;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +16,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/partners")
 public class PartnerApiController {
 	private final PartnerFacade partnerFacade;
+	private final PartnerDtoMapper partnerDtoMapper;
 
 	@PostMapping
-	public CommonResponse registerPartner(PartnerDto.RegisterRequest request) {
-		var command = request.toCommand();
+	public CommonResponse registerPartner(@RequestBody @Valid PartnerDto.RegisterRequest request) {
+		var command = partnerDtoMapper.of(request);
 		var partnerInfo = partnerFacade.registerPartner(command);
 		var response = new PartnerDto.RegisterResponse(partnerInfo);
 		return CommonResponse.success(response);
